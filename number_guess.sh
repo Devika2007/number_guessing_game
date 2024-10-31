de6f6a0860c8cae69d25bb6fe280698252bbde4c
Devika S <devikasathishkumar784@gmail.com>
2024-10-31 18:54:00 +0000

    your new message starting with the required prefix

--- number_guess.sh
+++ number_guess.sh
@@ -5,6 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # Function to handle user input and guessing
 function start_game() {
+  # Generate a random secret number between 1 and 1000
   SECRET_NUMBER=$((RANDOM % 1000 + 1))
   NUMBER_OF_GUESSES=0
 
@@ -26,30 +27,13 @@ function start_game() {
     elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
       echo "It's lower than that, guess again:"
     else
+      # User guessed the secret number
       echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-      update_user_stats "$USERNAME" "$NUMBER_OF_GUESSES"
+      # Update user stats in the database
+      UPDATE_STATS_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = LEAST(best_game, $NUMBER_OF_GUESSES) WHERE username = '$USERNAME'")
       break
     fi
   done
-}
-
-# Function to update user statistics
-function update_user_stats() {
-  local USERNAME=$1
-  local NUMBER_OF_GUESSES=$2
-  
-  USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME'")
-  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
-
-  # Increment games played
-  GAMES_PLAYED=$((GAMES_PLAYED + 1))
-  
-  if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
-    BEST_GAME=$NUMBER_OF_GUESSES
-  fi
-  
-  # Update user statistics in the database
-  $PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME';"
 }
 
 # Prompt for username
@@ -62,11 +46,12 @@ USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$
 if [[ -z $USER_DATA ]]; then
   # New user
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  INSERT_RESULT=$($PSQL "INSERT INTO users (username, games_played, best_game) VALUES ('$USERNAME', 0, NULL)")
+  # Insert new user into the database
+  INSERT_RESULT=$($PSQL "INSERT INTO users (username, games_played, best_game) VALUES ('$USERNAME', 1, 100)")
   start_game
 else
   # Existing user
-  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  IFS=" | " read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   start_game
 fi
